22a8ab2ecaba8da3c840e7a0b093f23314395a2a
AmitWin <64320889+AmitWin@users.noreply.github.com>
2021-04-24 22:42:05 +0300

    Added partly board and piece classes

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-for i in range(10):
-    print("hello")
+import pygame as pg
+from board import Board
+
+board = Board()
+
+win = pg.display.set_mode((board.width, board.height))
+pg.display.set_caption("Checkers")
+
+
+def draw():
+    board.draw(win)
+    pg.display.update()
+
+#Main Loop
+def main():
+    run = True
+    clock = pg.time.Clock()
+
+    while run:
+        clock.tick(27)
+
+        keys = pg.key.get_pressed()
+        for e in pg.event.get():
+            if e.type == pg.QUIT or keys[pg.K_ESCAPE]:
+                run = False
+
+            if e.type == pg.MOUSEBUTTONDOWN:
+                pass
+
+        draw()
+
+main()
---
+++ piece.py
@@ -0,0 +1,5 @@
+import pygame as pg
+
+class Piece():
+    def __init__(self):
+        self.diameter = 70
