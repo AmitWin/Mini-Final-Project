699a3a5ac04ad104d3fe67c7f605c54ac246db72
AmitWin <64320889+AmitWin@users.noreply.github.com>
2021-04-26 08:15:00 +0300

    worked a little bit on the project

--- board.py
+++ board.py
@@ -1,23 +1,34 @@
 import pygame as pg
+from piece import Piece
 
-black = [0, 0, 0]
-white = [255, 228, 196]
+yellow = [154, 205, 50]
+blue = [65, 105, 225]
 
 
 class Board():
     def __init__(self):
-        self.width = 800
-        self.height = 800
+        self.width = 600
+        self.height = 600
         self.rows = 8
         self.cols = 8
         self.sqr_width = self.width / self.rows
         self.sqr_height = self.height / self.cols
-        #self.board =
+        self.board = None
+
+    def initiateBoard(self):
+        self.board = [[],
+                      [],
+                      [],
+                      [],
+                      [],
+                      [],
+                      [],
+                      []]
 
     def draw(self, win):
         for i in range(self.rows):
             for j in range(self.cols):
                 if (i + j) % 2 == 0:
-                    pg.draw.rect(win, black, (self.sqr_width * i, self.sqr_height * j, self.sqr_width, self.sqr_height))
+                    pg.draw.rect(win, yellow, (self.sqr_width * i, self.sqr_height * j, self.sqr_width, self.sqr_height))
                 elif (i + j) % 2 == 1:
-                    pg.draw.rect(win, white, (self.sqr_width * i, self.sqr_height * j, self.sqr_width, self.sqr_height))
+                    pg.draw.rect(win, blue, (self.sqr_width * i, self.sqr_height * j, self.sqr_width, self.sqr_height))
--- piece.py
+++ piece.py
@@ -1,5 +1,17 @@
 import pygame as pg
 
+blackColor = [0, 0, 0]
+whiteColor = [255, 255, 255]
+
 class Piece():
-    def __init__(self):
+    def __init__(self, location):
         self.diameter = 70
+        self.black = False
+        self.white = False
+        self.location = location
+
+    def draw(self, win):
+        if self.black:
+            pg.draw.circle(win, blackColor, self.location, self.diameter)
+        elif self.white:
+            pg.draw.circle(win, blackColor, self.location, self.diameter)
